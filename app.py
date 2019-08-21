35b8d503b763ae81cd3a3bcee56ed87a07dae94f
Alan <alanh7@uci.edu>
2019-08-21 14:18:40 -0700

    -cleaned up the code -need to transition to plotly dash

--- app.py
+++ app.py
@@ -1,89 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-
-
 import pandas as pd
 import plotly.graph_objects as go
 
 df=pd.read_csv('./data/wide_budget.csv')
-yeardf, pwdf, devservicedf, entdf, electdf, internaldf, safetydf, comservicedf, econdf, citywidedf = [], [], [], [], [], [], [], [], [], []
-totaldf = []
 
-for col in range(1, len(df.columns)):
-    yeardf.append(df.columns[col]) #x axis, year
-    pwdf.append(df.iloc[0][col]) #y axis, public works
-    devservicedf.append(df.iloc[1][col]) # y axis, development services
-    entdf.append(df.iloc[2][col]) # y axis, enterprise 
-    electdf.append(df.iloc[3][col]) # y axis, elected and appointed
-    internaldf.append(df.iloc[4][col]) #y axis, internal services
-    safetydf.append(df.iloc[5][col]) # y axis, public safety
-    comservicedf.append(df.iloc[6][col]) # y axis, community services
-    econdf.append(df.iloc[7][col]) # y axis, economic development
-    citywidedf.append(df.iloc[8][col]) #y axis, city wide activities
-    totaldf.append(df.iloc[9][col]) # y axis, total
-
-#print(yeardf)
-#print (pwdf)
-#print(df.iloc[9][:])
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=yeardf, y=citywidedf, name='city wide activities', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=econdf, name='economic development', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=comservicedf, name='community services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=safetydf, name='public safety', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=internaldf, name='internal services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=electdf, name='elected and appointed', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=entdf, name='enterprise', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=devservicedf, name='development services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=pwdf, name='public works', stackgroup='one')) 
-#fig.add_trace(go.Scatter(x=yeardf, y=totaldf, name='total'))
+
+for i in range(len(df.columns)-1):
+    fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one'))
 
 fig.show()
 
 
-# In[6]:
+fig = go.Figure()
+
+for i in range(len(df.columns)-1):
+    fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
+    
+fig.show()
 
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=yeardf, y=citywidedf, name='city wide activities'))
-fig.add_trace(go.Scatter(x=yeardf, y=econdf, name='economic development'))
-fig.add_trace(go.Scatter(x=yeardf, y=comservicedf, name='community services'))
-fig.add_trace(go.Scatter(x=yeardf, y=safetydf, name='public safety'))
-fig.add_trace(go.Scatter(x=yeardf, y=internaldf, name='internal services'))
-fig.add_trace(go.Scatter(x=yeardf, y=electdf, name='elected and appointed'))
-fig.add_trace(go.Scatter(x=yeardf, y=entdf, name='enterprise'))
-fig.add_trace(go.Scatter(x=yeardf, y=devservicedf, name='development services'))
-fig.add_trace(go.Scatter(x=yeardf, y=pwdf, name='public works')) 
-#fig.add_trace(go.Scatter(x=yeardf, y=totaldf, name='total'))
+
+for i in range(len(df.columns)-1):
+    fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one', groupnorm='percent'))
 
 fig.show()
 
 
-# In[7]:
-
-
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=yeardf, y=citywidedf, name='city wide activities', stackgroup='one', groupnorm='percent'))
-fig.add_trace(go.Scatter(x=yeardf, y=econdf, name='economic development', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=comservicedf, name='community services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=safetydf, name='public safety', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=internaldf, name='internal services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=electdf, name='elected and appointed', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=entdf, name='enterprise', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=devservicedf, name='development services', stackgroup='one'))
-fig.add_trace(go.Scatter(x=yeardf, y=pwdf, name='public works', stackgroup='one')) 
-#fig.add_trace(go.Scatter(x=yeardf, y=totaldf, name='total'))
+
+for i in range(len(df.columns)-1):
+    fig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
 
 fig.show()
 
 
-# In[8]:
+fig = go.Figure()
 
+for i in range(len(df.columns)-1):
+    fig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
 
-df['2019-20 Proposed Budget']
-df.iloc[0][0]
+fig.update_layout(barmode='stack')
+fig.show()
+
 
 labels=[]
 values=[]
@@ -103,19 +66,5 @@ fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 fig.show()
 
 
-# In[9]:
 
-
-fig = go.Figure()
-fig.add_trace(go.Bar(x=yeardf, y=citywidedf, name='city wide activities'))
-fig.add_trace(go.Bar(x=yeardf, y=econdf, name='economic development'))
-fig.add_trace(go.Bar(x=yeardf, y=comservicedf, name='community services'))
-fig.add_trace(go.Bar(x=yeardf, y=safetydf, name='public safety'))
-fig.add_trace(go.Bar(x=yeardf, y=internaldf, name='internal services'))
-fig.add_trace(go.Bar(x=yeardf, y=electdf, name='elected and appointed'))
-fig.add_trace(go.Bar(x=yeardf, y=entdf, name='enterprise'))
-fig.add_trace(go.Bar(x=yeardf, y=devservicedf, name='development services'))
-fig.add_trace(go.Bar(x=yeardf, y=pwdf, name='public works')) 
-fig.update_layout(barmode='stack')
-fig.show()
 
