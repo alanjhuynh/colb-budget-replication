6c08580523c29b2001e69fafe16ea02c81b9a1c5
Alan <alanh7@uci.edu>
2019-09-05 11:54:48 -0700

    - added buttons/tabs and table - removed output from jupyter notebook

--- app.py
+++ app.py
@@ -4,14 +4,18 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly
 import plotly.graph_objects as go
 
 df=pd.read_csv('./data/wide_budget.csv')
 
+
 def clean_column_name(x):
     return x.replace(' ','_').lower()
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -74,27 +78,67 @@ for i in range(len(df['2019-20 Proposed Budget'])-1):
 pieFig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 #pieFig.show()
 
+stackedLineFig.update_layout(height=600)
+
 
 app.layout = html.Div(children=[
-    html.Div(children=[
-        dcc.Graph(
-            figure = stackedLineFig
-        ),
-        dcc.Graph(
-            figure = lineFig
-        ),
-        dcc.Graph(
-            figure = percentLineFig
-        ),
-        dcc.Graph(
-            figure = groupedBarFig
-        ),
-        dcc.Graph(
-            figure = stackedBarFig
-        ),
-    ])
+    html.H1(children=[
+            'city of long beach budget'
+    ], style={'text-align' : 'center', 'font-family' : 'Verdana', 'padding-top' : '10px'}),
     
-])
+    html.Div(className='row', children=[
+        html.Div(className='nine columns', id = 'figDiv', children=[
+            dcc.Graph(
+                id = 'mainFig',
+                figure = stackedLineFig,
+            ),
+        ]),
+        
+        html.Div(className='three columns', id = 'controlDiv', children=[
+           html.Div([
+            dcc.Tabs(id="tabs", value='tab-1', vertical='true', style={'width':'250px'}, children=[
+                dcc.Tab(label='stacked line', value='tab-1'),
+                dcc.Tab(label='line', value='tab-2'),
+                dcc.Tab(label='percent line', value='tab-3'),
+                dcc.Tab(label='grouped bar', value='tab-4'),
+                dcc.Tab(label='stacked bar', value='tab-5'),
+            ]),
+           ])
+        ], style = {'padding-top' : '5%'})
+        
+    ]),
+    
+    html.Div(dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+    ), style = {'padding-top' : '2%',
+                'padding-bottom' : '5%'})
+    
+], style={
+    'padding-right' : '5%',
+    'padding-left' : '5%'
+})
+
+
+@app.callback(Output('mainFig', 'figure'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        figure=stackedLineFig
+        return figure
+    elif tab == 'tab-2':
+        figure=lineFig
+        return figure
+    elif tab == 'tab-3':
+        figure=percentLineFig
+        return figure
+    elif tab == 'tab-4':
+        figure=groupedBarFig
+        return figure
+    elif tab == 'tab-5':
+        figure=stackedBarFig
+        return figure
 
 
 if __name__ == '__main__':
