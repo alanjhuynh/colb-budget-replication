6b8f2edd5dc902348520ce28f160dec3d20f8bcd
Alan <alanh7@uci.edu>
2020-01-28 13:08:08 -0800

    updated layout and css

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,11 +8,7 @@ import plotly
 import plotly.graph_objects as go
 
 df=pd.read_csv('./data/wide_budget.csv')
-
-
-def clean_column_name(x):
-    return x.replace(' ','_').lower()
-
+entDf=pd.read_csv('./data/enterprise_narrow1_budget.csv')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -28,121 +21,166 @@ colors = {
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+###################################################################
 
-stackedLineFig = go.Figure()
+def clean_column_name(x):
+    return x.replace(' ','_').lower()
 
-for i in range(len(df.columns)-1):
-    stackedLineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one'))
-#stackedLineFig.show()
-
-
-lineFig = go.Figure()
-
-for i in range(len(df.columns)-1):
-    lineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))    
-#lineFig.show()
-
-
-percentLineFig = go.Figure()
-
-for i in range(len(df.columns)-1):
-    percentLineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one', groupnorm='percent'))
-#percentLineFig.show()
-
-
-groupedBarFig = go.Figure()
-
-for i in range(len(df.columns)-1):
-    groupedBarFig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
-#groupedBarFig.show()
-
-
-stackedBarFig = go.Figure()
-
-for i in range(len(df.columns)-1):
-    stackedBarFig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
-stackedBarFig.update_layout(barmode='stack')
-#fig.show()
-
-
-labels=[]
-values=[]
-
-for i in range(len(df['2019-20 Proposed Budget'])-1):
-    labels.append(df.iloc[i][0])
-    values.append((df['2019-20 Proposed Budget'][i]))
-    values[i] = values[i].replace(',','')
-    values[i] = values[i].replace('$','')
-    values[i] = int(values[i])
+def create_stacked_line(df):
+    stackedLineFig = go.Figure()
     
-pieFig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-#pieFig.show()
+    for i in range(len(df.index)-1):
+        stackedLineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one'))
+    
+    return stackedLineFig
 
-stackedLineFig.update_layout(height=600)
+def create_line(df):
+    lineFig = go.Figure()
 
+    for i in range(len(df.index)-1):
+        lineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
+        
+    return lineFig
+
+def create_percent_line(df):
+    percentLineFig = go.Figure()
+
+    for i in range(len(df.index)-1):
+        percentLineFig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0], stackgroup='one', groupnorm='percent'))
+
+    return percentLineFig
+
+def create_grouped_bar(df):
+    groupedBarFig = go.Figure()
+
+    for i in range(len(df.index)-1):
+        groupedBarFig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
+        
+    return groupedBarFig
+
+def create_stacked_bar(df):
+    stackedBarFig = go.Figure()
+
+    for i in range(len(df.index)-1):
+        stackedBarFig.add_trace(go.Bar(x=df.columns[1:], y=df.iloc[i][1:], name=df.iloc[i][0]))
+        stackedBarFig.update_layout(barmode='stack')
+        
+    return stackedBarFig
+
+def create_pie(df):
+    labels=[]
+    values=[]
+
+    for i in range(len(df['2019-20 Proposed Budget'])-1):
+        labels.append(df.iloc[i][0])
+        values.append((df['2019-20 Proposed Budget'][i]))
+        values[i] = values[i].replace(',','')
+        values[i] = values[i].replace('$','')
+        values[i] = int(values[i])
+    
+    pieFig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    
+    return pieFig
+
+def create_all(df):
+    figure = create_stacked_line(df)
+    figure = create_line(df)
+    figure = create_percent_line(df)
+    figure = create_grouped_bar(df)
+    figure = create_stacked_bar(df)
+
+stackedLineFig = create_stacked_line(df)
+#stackedLineFig.update_layout(height=100%)
+#stackedLineFig.update_layout(plot_bgcolor='black', paper_bgcolor='white')
+
+###################################################################
 
 app.layout = html.Div(children=[
-    html.H1(children=[
-            'city of long beach budget'
-    ], style={'text-align' : 'center', 'font-family' : 'Verdana', 'padding-top' : '10px'}),
-    
-    html.Div(className='row', children=[
-        html.Div(className='nine columns', id = 'figDiv', children=[
-            dcc.Graph(
-                id = 'mainFig',
-                figure = stackedLineFig,
-            ),
-        ]),
-        
-        html.Div(className='three columns', id = 'controlDiv', children=[
-           html.Div([
-            dcc.Tabs(id="tabs", value='tab-1', vertical='true', style={'width':'250px'}, children=[
-                dcc.Tab(label='stacked line', value='tab-1'),
-                dcc.Tab(label='line', value='tab-2'),
-                dcc.Tab(label='percent line', value='tab-3'),
-                dcc.Tab(label='grouped bar', value='tab-4'),
-                dcc.Tab(label='stacked bar', value='tab-5'),
+    html.Div(className='fullscreen', children=[
+        html.Div(className='fullscreen30', children=[
+            html.H3(className='sidepanel sp-title', children=[
+                'LONG BEACH BUDGET'
             ]),
-           ])
-        ], style = {'padding-top' : '5%'})
-        
-    ]),
-    
-    html.Div(dash_table.DataTable(
-    id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),
-    ), style = {'padding-top' : '2%',
-                'padding-bottom' : '5%'})
-    
-], style={
-    'padding-right' : '5%',
-    'padding-left' : '5%'
-})
+            html.P(className='sidepanel sp-text', children=[
+                'Select different graph types or filter specific departments.'
+            ]),
+            html.Div(className='sp-dropdown', children=[
+                dcc.Dropdown(
+                    id='deptDropdown',
+                    options=[
+                        {'label': 'Departments', 'value': 'dept'},
+                        {'label': 'Enterprise', 'value': 'ent'},
+                    ],
+                    value='dept'
+                )
+            ]),
+            html.Div(className='sp-dropdown', children=[
+                dcc.Tabs(id="tabs", value='tab-1', vertical=True, style={'width':'22.5vw'},children=[
+                    dcc.Tab(className='sp-tab-text', label='Stacked Line', value='tab-1'),
+                    dcc.Tab(className='sp-tab-text', label='Line', value='tab-2'),
+                    dcc.Tab(className='sp-tab-text', label='Percent Line', value='tab-3'),
+                    dcc.Tab(className='sp-tab-text', label='Grouped Bar', value='tab-4'),
+                    dcc.Tab(className='sp-tab-text', label='Stacked Bar', value='tab-5'),
+                    dcc.Tab(className='sp-tab-text', label='Pie', value='tab-6')
+                ])
+            ]),
+            dcc.Markdown(className='sidepanel sp-text', children=[
+                "Created by [Alan Huynh](https://alanjhuynh.com/)\n",
+                "Source: [City of Long Beach](http://www.longbeach.gov/finance/city-budget-and-finances/budget/budget-information/)"
+            ]),
+        ]),
+        html.Div(className='fullscreen70-top', children=[
+            html.Div(className='fullscreen70-graph', children=[
+                dcc.Graph(
+                    id = 'mainFig',
+                    figure = stackedLineFig,
+                ),
+            ]),
+        ]),
+        html.Div(className='fullscreen70-bottom', children=[
+            html.Div(className='fullscreen70-chart', children=[
+                dash_table.DataTable(
+                    id='table',
+                    columns=[{"name": i, "id": i} for i in df.columns],
+                    data=df.to_dict('records'),
+                )
+            ])
+        ]),
+    ])
+])
 
+###################################################################
 
 @app.callback(Output('mainFig', 'figure'),
-              [Input('tabs', 'value')])
-def render_content(tab):
+              [Input('tabs', 'value'),
+               Input('deptDropdown', 'value')])
+def render_content(tab, value):
+    if value == 'ent':
+        currDf = entDf
+    elif value == 'dept':
+        currDf = df
+        
     if tab == 'tab-1':
-        figure=stackedLineFig
+        figure=create_stacked_line(currDf)
         return figure
     elif tab == 'tab-2':
-        figure=lineFig
+        figure=create_line(currDf)
         return figure
     elif tab == 'tab-3':
-        figure=percentLineFig
+        figure=create_percent_line(currDf)
         return figure
     elif tab == 'tab-4':
-        figure=groupedBarFig
+        figure=create_grouped_bar(currDf)
         return figure
     elif tab == 'tab-5':
-        figure=stackedBarFig
+        figure=create_stacked_bar(currDf)
+        return figure
+    elif tab == 'tab-6':
+        figure=create_pie(currDf)
         return figure
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True)
+    app.run_server(debug=True)
 
     app.run_server(host='0.0.0.0', port = 8050)
-
